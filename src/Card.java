65f67170868679bd21f75f92504f9911de978c64
jacob baber <jacob.baber@ou.edu>
2021-04-01 21:59:23 -0500

    Skeleton of Card.java complete

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,38 @@
 
 public class Card {
+	
+	private Rank rank;
+	private Suit suit;
+	
+	public Card(Rank rank, Suit suit) {
+		
+	}
+	
+	public int compareTo(Card card) {
+		
+	}
+	
+	public boolean equals(Object obj) {
+		
+	}
+	
+	public Rank getRank() {
+		return rank;
+	}
+	
+	public Suit getSuit() {
+		return suit;
+	}
+	
+	public int hashCode() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+	}
+	
+	
 
 }
