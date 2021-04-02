606de7904345a3c298fa17c11935acdc00235608
jacob baber <jacob.baber@ou.edu>
2021-04-01 21:28:29 -0500

    Suit.java done

--- src/Suit.java
+++ src/Suit.java
@@ -1,5 +1,20 @@
 
 public enum Suit {
+	
+	CLUBS("C"), DIAMONDS("D"), HEARTS("H"), SPADES("S");
+	
+	private String symbol;
+	
+
+	Suit(String string) {
+		symbol = string;
+	}
+	
+	@Override
+	public String toString() {
+		return symbol;
+		
+	}
 
 
 }
