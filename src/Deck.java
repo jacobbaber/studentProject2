c809dbc0b5c58dd71a8f1eb324f77689591065c0
jacob baber <jacob.baber@ou.edu>
2021-04-03 01:24:47 -0500

    Skeleton done

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Deck {
+	
+	private List<Card> cards = new ArrayList<Card>();
+	
+	public Deck() {
+		
+	}
+	
+	public Card draw() {
+		
+	}
 
+	public List<Card> draw(int count){
+		
+	}
+	
+	public void shuffle() {
+		
+	}
+	
+	public int size() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+	}
 }
