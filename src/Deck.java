f89823e60ff3b5bf77d4aa32335007d44f07c812
jacob baber <jacob.baber@ou.edu>
2021-04-03 01:45:21 -0500

    Constructor, size method, toString method, and draw() method completed

--- src/Deck.java
+++ src/Deck.java
@@ -6,10 +6,25 @@ public class Deck {
 	private List<Card> cards = new ArrayList<Card>();
 	
 	public Deck() {
+		for (Suit suitVal : Suit.values()) {
+			for (Rank rankVal : Rank.values()) {
+				cards.add(new Card(rankVal,suitVal));
+			}
+			
+		}
 		
 	}
 	
 	public Card draw() {
+		if (cards.isEmpty() == true) {
+			return null;
+		}
+		else {
+			 Card drawnCard = cards.get(0);
+			 cards.remove(0);
+			 return drawnCard;
+			
+		}
 		
 	}
 
@@ -22,11 +37,27 @@ public class Deck {
 	}
 	
 	public int size() {
+		return cards.size();
 		
 	}
 	
 	@Override
 	public String toString() {
+		
+		if (cards.isEmpty() == true) {
+			return "[]";
+		}
+		else {
+		String deck = "[";
+		for (Card card : cards) {
+			deck = deck + card.toString() + ", ";
+			
+		}
+		deck = deck.substring(0, deck.length() - 2);
+		deck = deck + "]";
+		
+		return deck;
+		}
 		
 	}
 }
