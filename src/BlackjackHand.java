dfb41a23e9e84ea3b35b6e68d3b26247e5ac45f4
jacob baber <jacob.baber@ou.edu>
2021-04-04 14:02:23 -0500

    Skeleton complete

--- src/BlackjackHand.java
+++ src/BlackjackHand.java
@@ -1,4 +1,40 @@
+import java.util.List;
+import java.util.Map;
 
 public class BlackjackHand {
+	
+	private Map<Rank, Integer> CARD_VALUES;
+	
+	private final int MAX_VALUE = 21;
+	
+	private List<Card> cards;
+	
+	public BlackjackHand(Card c1, Card c2) {
+		
+	}
+	
+	public void addCard(Card card) {
+		
+	}
+	
+	public Map<Rank, Integer> getCardValues(){
+		
+	}
+	
+	public List<Card> getCards(){
+		
+	}
+	
+	public int getValue() {
+		
+	}
 
+	public int size() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+	}
 }
