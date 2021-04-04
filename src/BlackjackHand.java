953984413ad135847bdf583a9cbc09894ca5a65d
jacob baber <jacob.baber@ou.edu>
2021-04-04 15:24:28 -0500

    Working on all methods

--- src/BlackjackHand.java
+++ src/BlackjackHand.java
@@ -1,27 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BlackjackHand {
 	
-	private Map<Rank, Integer> CARD_VALUES;
+	private static Map<Rank, Integer> CARD_VALUES = createCardValues();
 	
 	private final int MAX_VALUE = 21;
 	
-	private List<Card> cards;
+	private List<Card> cards = new ArrayList<Card>();
 	
 	public BlackjackHand(Card c1, Card c2) {
-		
+		cards.add(c1);
+		cards.add(c2);
 	}
 	
 	public void addCard(Card card) {
+		if (getValue() < MAX_VALUE) {
+		cards.add(card);
+		}
 		
 	}
 	
-	public Map<Rank, Integer> getCardValues(){
+	public static Map<Rank, Integer> getCardValues(){
+		return createCardValues();
 		
 	}
 	
 	public List<Card> getCards(){
+		List<Card> copy = cards;
+		return copy;
 		
 	}
 	
@@ -30,11 +39,30 @@ public class BlackjackHand {
 	}
 
 	public int size() {
+		return cards.size();
 		
 	}
 	
 	@Override
 	public String toString() {
 		
+	}
+	
+	public static Map<Rank, Integer> createCardValues(){
+		Map<Rank, Integer> vals = new HashMap<Rank, Integer>();
+		vals.put(Rank.TWO, Integer.valueOf(2));
+		vals.put(Rank.THREE, Integer.valueOf(3));
+		vals.put(Rank.FOUR, Integer.valueOf(4));
+		vals.put(Rank.FIVE, Integer.valueOf(5));
+		vals.put(Rank.SIX, Integer.valueOf(6));
+		vals.put(Rank.SEVEN, Integer.valueOf(7));
+		vals.put(Rank.EIGHT, Integer.valueOf(8));
+		vals.put(Rank.NINE, Integer.valueOf(9));
+		vals.put(Rank.TEN, Integer.valueOf(10));
+		vals.put(Rank.JACK, Integer.valueOf(10));
+		vals.put(Rank.QUEEN, Integer.valueOf(10));
+		vals.put(Rank.KING, Integer.valueOf(10));
+		vals.put(Rank.ACE, Integer.valueOf(11));
+		return vals;
 	}
 }
