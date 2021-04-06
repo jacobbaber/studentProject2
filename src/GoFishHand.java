f16e7ce86e47d940d50076178a21f27dc88d210e
jacob baber <jacob.baber@ou.edu>
2021-04-05 23:59:15 -0500

    Working with GoFishHand methods

--- src/GoFishHand.java
+++ src/GoFishHand.java
@@ -1,4 +1,5 @@
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -12,27 +13,47 @@ public class GoFishHand {
 
 	private Map<Rank, Set<Card>> cards = new HashMap<Rank, Set<Card>>();
 	
+	private int cardsInHand = 0;
+	
 	public GoFishHand(Collection<Card> cards){
+		if (cards.isEmpty() == true) {
+			throw new IllegalArgumentException();
+		}
+		addCards(cards);
 		
 	}
 	
 	public void addCard(Card card) {
+		Set<Card> newCardSet = new HashSet<Card>();
+		Set<Card> cardSet = cards.getOrDefault(card.getRank(), newCardSet);
+		cardSet.add(card);
+		cards.put(card.getRank(), cardSet);
+		++cardsInHand;
 		
 	}
 	
 	public void addCards(Collection<Card> cards) {
+		for (Card c : cards) {
+			addCard(c);
+		}
 		
 	}
 	
 	public Set<Rank> getBooks(){
-		
+		Set<Rank> copy = new HashSet<Rank>(books);
+		return copy;
 	}
 	
 	public Map<Rank, Set<Card>> getCards(){
+		Map<Rank, Set<Card>> copy = new HashMap<Rank, Set<Card>>(cards);
+		return copy;
 		
 	}
 	
 	public Map<Rank, Integer> getRankCounts(){
+		
+			
+		
 		
 	}
 	
@@ -41,6 +62,7 @@ public class GoFishHand {
 	}
 	
 	public int size() {
+		return cardsInHand;
 		
 	}
 	
